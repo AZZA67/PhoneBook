a405d8449d3c97e588c325926c1f6265ea9b261d
AZZA67 <azzabahaa99@gmail.com>
2022-08-11 17:26:27 +0200

    test

--- PhoneBook.Application/Profiles/AutoMapperProfile.cs
+++ PhoneBook.Application/Profiles/AutoMapperProfile.cs
@@ -1,7 +1,6 @@
 ﻿using AutoMapper;
 using PhoneBook.Application.Features.PhoneBook.Queries.GetPhoneBooksByUserId;
 using PhoneBook.Domain;
-
 namespace PhoneBook.Application.Profiles
 {
     public class AutoMapperProfile :Profile
@@ -9,6 +8,7 @@ namespace PhoneBook.Application.Profiles
         public AutoMapperProfile()
         {
             CreateMap<phoneBook, GetPhoneBookByUserIdViewModel>().ReverseMap();
+            CreateMap<CreateUserViewModel, ApplicationUser>().ReverseMap();
         }
        
     }
